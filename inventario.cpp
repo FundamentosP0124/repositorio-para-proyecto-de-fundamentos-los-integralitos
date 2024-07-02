b0e37b97727271f7097abbc58fb0395ddb2f0df2
David Ventura <00089724@uca.edu.sv>
2024-07-02 00:19:52 -0600

    Agregue el inventario y la barra de carga entre otras cosas :meo:

--- inventario.cpp
+++ inventario.cpp
@@ -1,57 +1,32 @@
 #include <iostream>
-#include <time.h>
-#include <string>
+#include <unistd.h> // Para usleep en sistemas Unix-like (Linux, macOS)
+
 using namespace std;
 
-int main()
-{
-    string objetosDisponibles[] = {"cuchillo", "fosforos", "radio", "alimentos", "bebidas", "lupa"};
-    string suministros[4];
-    char a;
-    int objetosRecolectados = 0;
-    srand(static_cast<unsigned int>(time(0)));
+void mostrarBarraDeCarga(int progreso, int total) {
+    float percent = (float)progreso / total;
+    int barWidth = 70;
 
-    while (objetosRecolectados < 4)
-    {
-        cout << "Quieres recolectar un objeto? (y/n): ";
-        cin >> a;
-
-        if (a == 'y')
-        {
-            int indiceAleatorio;
-            bool objetoRepetido;
-            do
-            {
-                objetoRepetido = false;
-                indiceAleatorio = rand() % 6;
-
-                for (int i = 0; i < objetosRecolectados; i++)
-                {
-                    if (suministros[i] == objetosDisponibles[indiceAleatorio])
-                    {
-                        objetoRepetido = true;
-                        break;
-                    }
-                }
-            } while (objetoRepetido);
-
-            suministros[objetosRecolectados++] = objetosDisponibles[indiceAleatorio];
-            cout << "Recolectaste: " << suministros[objetosRecolectados - 1] << endl;
-        }
-        else
-        {
-            cout << "No has recolectado ningun objeto." << endl;
-            break;
-        }
+    cout << "[";
+    int pos = barWidth * percent;
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) cout << "=";
+        else if (i == pos) cout << ">";
+        else cout << " ";
     }
+    cout << "] " << int(percent * 100.0) << " %\r";
+    cout.flush();
+}
 
-    if (objetosRecolectados > 0) {
-        cout << "Objetos recolectados: ";
-        for (int i = 0; i < objetosRecolectados; i++) {
-            cout << suministros[i] << (i < objetosRecolectados - 1 ? ", " : "");
-        }
-        cout << endl;
+int main() {
+    int total = 100;
+    
+    for (int i = 0; i <= total; ++i) {
+        mostrarBarraDeCarga(i, total);
+        // Simulación de trabajo
+        usleep(100000); // Espera 100000 microsegundos (0.1 segundos)
     }
+    cout << endl;
 
     return 0;
 }
